03ec72a10a9cce93f21c87e41726d9a1391674c3
alecpietersz <80108884+alecpietersz@users.noreply.github.com>
2022-11-23 15:08:27 +0100

    LeastSquares implemented

--- DemandForecast.py
+++ DemandForecast.py
@@ -1,11 +1,23 @@
+
+from gurobipy import *
+from numpy import *
 from openpyxl import *
+from time import *
+import numpy as np
 
 class AirportPair:
-    def __init__(self,origin, destination, distance, demand):
+    def __init__(self,origin, destination, distance, demand2030):
         self.From       = origin
         self.To         = destination
         self.Distance   = distance
-        self.Demand     = demand
+        self.Demand2030 = demand2030
+
+class DemandAirportPair:
+    def __init__(self,origin, destination, demand2020):
+        self.From       = origin
+        self.To         = destination
+        self.Demand2020 = demand2020
+
 
 class Airport:
     def __init__(self,name, icao, latitude, longitude, runway_length, population, gdp, hub):
@@ -18,31 +30,101 @@ class Airport:
         self.GDP            = gdp        
         self.Hub            = hub
 
+class Aircraft:
+    def __init__(self,name, cask, lf, seats, speed, lto, bt, fleet, yields, ranges, cost):
+        self.Name   = name
+        self.CASK   = cask
+        self.LF     = lf
+        self.Seats  = seats
+        self.Speed  = speed
+        self.LTO    = lto/60
+        self.BT     = bt*7
+        self.Fleet  = fleet
+        self.Yield  = yields
+        self.Range  = ranges
+        self.Cost   = cost
 
 
-wb = load_workbook("Group_16_Aircraft_info.csv", read_only=True)
-List_aircraft_info = tuple(wb["Group_16_Aircraft_info"].iter_rows())
+if __name__ == '__main__':
+    #=================================================================================================
+    # Input excel file with arcs data (sheet1) and commodities data (sheet2)
+    fuel_price = 5.22
+    AirportPairs    = []
+    Aircrafts       = []
+    Airports        = []
+    DemandPairs     = []
 
-wb = load_workbook("Group_16_Airport_info.csv", read_only=True)
-List_our_airports = tuple(wb["Group_16_Airport_info"].iter_rows())
+    wb = load_workbook("Aircraft_info.xlsx", read_only=True)
+    List_aircraft_info = tuple(wb["Aircraft_info"].iter_rows())
 
-wb = load_workbook("Group_16_Demand.csv", read_only=True)
-List_demand_pairs = tuple(wb["Group_16_Demand"].iter_rows())
+    wb = load_workbook("Group_16_Airport_info.xlsx", read_only=True)
+    List_airport_info = tuple(wb["Group_16_Airport_info"].iter_rows())
 
-wb = load_workbook("Group_16_Distances.csv", read_only=True)
-List_demand_pairs = tuple(wb["Group_16_Distances"].iter_rows())
+    wb = load_workbook("Group_16_Demand.xlsx", read_only=True)
+    List_demand_forecast_data = tuple(wb["Group_16_Demand"].iter_rows())
 
-wb = load_workbook("Group_16_Annual_growth.csv", read_only=True)
-List_demand_pairs = tuple(wb["Group_16_Annual_growth"].iter_rows())
+    wb = load_workbook("Group_16_Distances.xlsx", read_only=True)
+    List_airport_distances = tuple(wb["Group_16_Distances"].iter_rows())
+
+    wb = load_workbook("Group_16_Annual_Growth.xlsx", read_only=True)
+    List_annual_growth = tuple(wb["Group_16_Annual_growth"].iter_rows())
+
+    # origin, destination, distance, demand2020, demand2030
+    for i, origin in enumerate(List_demand_forecast_data[1:]):
+        for j, destination in enumerate(origin[1:]):
+            new_pair = DemandAirportPair(List_demand_forecast_data[i+1][0].value,List_demand_forecast_data[0][j+1].value,int(List_demand_forecast_data[i+1][j+1].value))
+            DemandPairs.append(new_pair)
+    
+    # name, icao, latitude, longitude, runway_length, population, gdp, hub
+    for (name, icao, latitude, longitude, runway_length, population, gdp, hub) in List_airport_info[1:]:
+        new = Airport(name.value, icao.value, latitude.value, longitude.value, int(runway_length.value), int(population.value), gdp.value, int(hub.value))
+        Airports.append(new)
+
+    for i, origin in enumerate(List_airport_distances[1:]):
+        for j, destination in enumerate(origin[1:]):
+            new_pair = AirportPair(List_airport_distances[i+1][0].value,List_airport_distances[0][j+1].value,int(List_airport_distances[i+1][j+1].value), None)
+            AirportPairs.append(new_pair)
+
+    # for pair in AirportPairs:
+    #     print(vars(pair))
+    # print("")    
+    # for pair in DemandPairs:
+    #     print(vars(pair))
+
+    
+    X = np.zeros((len(DemandPairs),4))
+    # print(X)
+    for i in range(len(DemandPairs)):
+        for j in range(4):
+            if j == 0:
+                X[i,j] = 1
+            elif j == 1:
+                # next(airp for airp in Airports if airp.Name == AirportPairs[m].From).Population
+                X[i,j] = log(next(airp for airp in Airports if airp.ICAO == DemandPairs[i].From).Population*
+                            next(airp for airp in Airports if airp.ICAO == DemandPairs[i].To).Population)  
+            elif j == 2:
+                X[i,j] = log(next(airp for airp in Airports if airp.ICAO == DemandPairs[i].From).GDP*
+                            next(airp for airp in Airports if airp.ICAO == DemandPairs[i].To).GDP)  
+            elif j == 3:
+                X[i,j] = log(next(airppair for airppair in AirportPairs if (airppair.From == DemandPairs[i].From and airppair.To == DemandPairs[i].To)).Distance*
+                            fuel_price) 
+
+    X = np.delete(X, np.where(X < 0)[0], axis=0)
+
+    y = np.zeros(len(DemandPairs))
+    for i in range(len(y)):
+        y[i] = log(DemandPairs[i].Demand2020)
+
+    y = np.delete(y, np.where(y < 0)[0], axis=0)
+   
 
 
-for i, origin in enumerate(List_airport_demands[1:]):
-    for j, destination in enumerate(origin[1:]):
-        new_pair = AirportPair(List_airport_demands[i+1][0].value,List_airport_demands[0][j+1].value,int(List_airport_distances[i+1][j+1].value),int(destination.value))
-        AirportPairs.append(new_pair)
+    solution = np.linalg.lstsq(X,y)[0]
 
-for (name,hub) in List_airports[1:]:
-    new = Airport(name.value,int(hub.value))
-    Airports.append(new)
+    print("k = ",solution[0])
+    print("b1 = ",solution[1])
+    print("b2 = ",solution[2])
+    print("b3 = ",solution[3])
+ 
 
     
